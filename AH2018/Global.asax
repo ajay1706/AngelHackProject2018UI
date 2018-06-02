b47338d08d5d38a7e376a322b764ba4a017edc5d
Ashish Vishwakarma <Hello@AshishVishwakarma.com>
2018-06-02 14:02:42 +0530

    added asp.net project

---
+++ AH2018/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AH2018.MvcApplication" Language="C#" %>
---
+++ AH2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AH2018.Startup))]
+namespace AH2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
